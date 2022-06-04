8bd55bf7ef13e07a9ab9f24308b2ba2aba331541
Ryan Newport <ryndesign@gmail.com>
2022-06-04 10:31:06 -0400

    Dist Folder

--- dist/inc/classes.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('classes/mysql.class.php');
-require_once('classes/helper.class.php');
-
-?>
--- dist/inc/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('global.inc.php');
-require_once('classes.inc.php');
-
-?>
