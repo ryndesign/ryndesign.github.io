a48c8edaad52923c56eed5b8115b1120c94a1198
Ryan Newport <ryndesign@gmail.com>
2025-11-18 09:12:59 -0500

    Meta

--- PixelPeople/main.js
+++ PixelPeople/main.js
@@ -747,8 +747,8 @@ homeShare.addEventListener('click', async ()=>{
   try{
     if (navigator.share) {
       await navigator.share({
-        title: "Pixel People - Character Guessing Game",
-        text: "Play Now and see how many you can name.",
+        title: "Pixel People - Guess Who?",
+        text: "Play Now! See how many you can name.",
         url
       });
       showResult("Shared successfully.","good");
