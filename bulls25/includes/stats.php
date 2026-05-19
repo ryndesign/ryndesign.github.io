81c6095270c13d705ee23530704bfc8314c54260
Ryan Newport <ryndesign@gmail.com>
2026-05-19 10:00:37 -0400

    2026

---
+++ bulls25/includes/stats.php
@@ -0,0 +1,34 @@
+<table id="example" class="stripe" style="width:100%">
+        <thead>
+            <tr>                
+                <th width="20%">Player</th>
+                <th>G</th>
+                <th>AB</th>
+                <th>H</th> 
+                <th>2B</th> 
+                <th>3B</th> 
+                <th>HR</th> 
+                <th>RBI</th> 
+                <th>BB</th>
+                <th>SO</th> 
+                <th>AVG</th> 
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Doug Dusty</td>
+                <td>5</td>
+                <td>22</td>
+                <td>12</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>3</td>
+                <td>0</td>
+                <td>2</td>
+                <td>0.545</td>
+            </tr>
+         
+            
+        </tbody>
+    </table>
